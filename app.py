f693510b084788cca93d15418f6b4ce8d700f684
Saifansari-ai <saifansari300b@gmail.com>
2025-09-13 05:55:35 +0530

    app for the Agentic RAG

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 from src.pipeline.pipeline import Pipeline
+from src.logger import logging
+
+logging.info("App stating ")
 
 st.set_page_config(page_title="Blog Search",page_icon=":mag_right:")
 st.header(":blue[Agentic RAG with LangGraph:] :green[Blog Search]")
+
+logging.info("app started")
+logging.info("setting up session state for credentials")
 
 if 'qdrant_host' not in st.session_state:
     st.session_state.qdrant_host = ""
@@ -11,5 +17,9 @@ if 'qdrant_api_key' not in st.session_state:
 if 'gemini_api_key' not in st.session_state:
     st.session_state.gemini_api_key = ""
 
+logging.info("Pipeline starting")    
+
 run = Pipeline()
 run.main()
+
+logging.info("Run complete")
