79d17b0319e151ac2c5a54269d6705c143fd4471
Saifansari-ai <saifansari300b@gmail.com>
2025-09-17 12:03:12 +0530

    setup

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup,find_packages
 setup(
     name="src",
     version="0.1",
+    author="your name",
+    author_email="youremail@gmail.com",
     packages=find_packages(),
 )
